999b333b200bb9cb47f4e69580408ca4eda66556
Mohammad Smiley <mohammad.smiley04@gmail.com>
2025-04-07 12:37:35 -0600

    Create Student model and migration

---
+++ x5_projects_module/app/Models/Student.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Student extends Model
+{
+    protected $fillable = [
+        'user_id',
+        'github_link',
+        'linkedin_link',
+        'is_completed',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
