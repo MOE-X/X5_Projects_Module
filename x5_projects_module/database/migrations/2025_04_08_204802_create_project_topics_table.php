c6f62e17826d81355b602b611cb24d52b35d74b5
Mohammad Smiley <mohammad.smiley04@gmail.com>
2025-04-12 14:26:56 -0600

    Fix grammer mistake

--- x5_projects_module/database/migrations/2025_04_08_204802_create_project_topics_table.php
+++ x5_projects_module/database/migrations/2025_04_08_204802_create_project_topics_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('project_topics', function (Blueprint $table) {
             $table->id();
-            $tabel->string('name')->unique();
+            $table->string('name')->unique();
             $table->timestamps();
         });
     }
